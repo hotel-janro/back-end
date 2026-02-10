370e28a34da46c0dbca4bedfa7d1c48c47b135cd
Kavindu Krishan <163849099+Krishan418@users.noreply.github.com>
2026-02-10 21:39:37 +0530

    backend initial setup with express

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 console.log("Hello, Node.js!");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hotel Management Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
